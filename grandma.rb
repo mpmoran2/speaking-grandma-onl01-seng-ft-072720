f38029bb31f07800a18b3aebb34aed1ed9169e4c
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 20:36:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,7 @@ def speak_to_gradma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   else if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
