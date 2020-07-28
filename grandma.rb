e5895d354dba2611c9ba4286d731dabe71a5194f
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 20:58:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,19 @@
 input_phrase = "Hi Ba-chan!"
 def speak_to_grandma(phrase)
-  if phrase =="I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
-   puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase
-   puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase 
-    puts "NO, NOT SINCE 1938!"
-  
-  end
+# respond with I love you too
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    # cant hear if you dont shout
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+  	#try again
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+    #can hear you once
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    # second time
+    else
+  		return "NO, NOT SINCE 1938!"
+    end
 end
