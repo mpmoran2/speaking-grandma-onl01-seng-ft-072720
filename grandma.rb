5bfab329b683b184781648e1bce254937a7f0283
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-28 20:50:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,8 +9,7 @@ def speak_to_grandma(phrase)
       return "NO, NOT SINCE 1938!"
     else phrase == phrase.upcase
   		return "NO, NOT SINCE 1938!"
-  	
-    end
+  	 end
 end
 
 speak_to_grandma("HELLO THERE")
